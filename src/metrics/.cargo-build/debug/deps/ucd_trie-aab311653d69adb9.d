cbf824a5d9c58fa4d58390fe0cbf633d8833b7de
ace-pm <agent-052a9fab-fb40-403b-bd08-d62bdb855c8b@letta.com>
2026-06-14 06:20:52 +0200

    git-auto-backup dirty snapshot
    repo: /home/mhugo/code/singularity-analysis-engine
    branch: main
    host: Mikki-Bunker
    time: 20260614T041950Z

--- src/metrics/.cargo-build/debug/deps/ucd_trie-aab311653d69adb9.d
+++
@@ -1,8 +0,0 @@
-/home/mhugo/code/singularity/packages/singularity-code-analysis/.cargo-build/debug/deps/ucd_trie-aab311653d69adb9.d: /home/mhugo/.cache/singularity/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ucd-trie-0.1.7/src/lib.rs /home/mhugo/.cache/singularity/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ucd-trie-0.1.7/src/owned.rs
-
-/home/mhugo/code/singularity/packages/singularity-code-analysis/.cargo-build/debug/deps/libucd_trie-aab311653d69adb9.rlib: /home/mhugo/.cache/singularity/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ucd-trie-0.1.7/src/lib.rs /home/mhugo/.cache/singularity/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ucd-trie-0.1.7/src/owned.rs
-
-/home/mhugo/code/singularity/packages/singularity-code-analysis/.cargo-build/debug/deps/libucd_trie-aab311653d69adb9.rmeta: /home/mhugo/.cache/singularity/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ucd-trie-0.1.7/src/lib.rs /home/mhugo/.cache/singularity/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ucd-trie-0.1.7/src/owned.rs
-
-/home/mhugo/.cache/singularity/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ucd-trie-0.1.7/src/lib.rs:
-/home/mhugo/.cache/singularity/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ucd-trie-0.1.7/src/owned.rs:
