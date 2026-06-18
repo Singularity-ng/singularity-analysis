b7a589ed6af6ed7dcd10f6700e58368d4f6b34a2
ace-pm <agent-052a9fab-fb40-403b-bd08-d62bdb855c8b@letta.com>
2026-06-18 10:56:01 +0200

    git-auto-backup dirty snapshot
    repo: /home/mhugo/code/singularity-analysis-engine
    branch: main
    host: Mikki-Bunker
    time: 20260618T085450Z

--- src/metrics/.cargo-build/debug/deps/shlex-8d318117cb544e20.d
+++
@@ -1,8 +0,0 @@
-/home/mhugo/code/singularity/packages/code_quality_engine/.cargo-build/debug/deps/shlex-8d318117cb544e20.d: /home/mhugo/.cache/singularity/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/shlex-1.3.0/src/lib.rs /home/mhugo/.cache/singularity/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/shlex-1.3.0/src/bytes.rs
-
-/home/mhugo/code/singularity/packages/code_quality_engine/.cargo-build/debug/deps/libshlex-8d318117cb544e20.rlib: /home/mhugo/.cache/singularity/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/shlex-1.3.0/src/lib.rs /home/mhugo/.cache/singularity/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/shlex-1.3.0/src/bytes.rs
-
-/home/mhugo/code/singularity/packages/code_quality_engine/.cargo-build/debug/deps/libshlex-8d318117cb544e20.rmeta: /home/mhugo/.cache/singularity/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/shlex-1.3.0/src/lib.rs /home/mhugo/.cache/singularity/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/shlex-1.3.0/src/bytes.rs
-
-/home/mhugo/.cache/singularity/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/shlex-1.3.0/src/lib.rs:
-/home/mhugo/.cache/singularity/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/shlex-1.3.0/src/bytes.rs:
