56b9e04925b08256518b805c02f1612e60451ae5
ace-pm <agent-052a9fab-fb40-403b-bd08-d62bdb855c8b@letta.com>
2026-07-01 03:48:17 +0200

    git-auto-backup dirty snapshot
    repo: /home/mhugo/code/singularity-analysis-engine
    branch: main
    host: Mikki-Bunker
    time: 20260701T014157Z

--- src/metrics/.cargo-build/debug/deps/fixedbitset-1f923cb1db34e7c6.d
+++
@@ -1,8 +0,0 @@
-/home/mhugo/code/singularity/packages/singularity-code-analysis/.cargo-build/debug/deps/fixedbitset-1f923cb1db34e7c6.d: /home/mhugo/.cache/singularity/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/fixedbitset-0.4.2/src/lib.rs /home/mhugo/.cache/singularity/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/fixedbitset-0.4.2/src/range.rs
-
-/home/mhugo/code/singularity/packages/singularity-code-analysis/.cargo-build/debug/deps/libfixedbitset-1f923cb1db34e7c6.rlib: /home/mhugo/.cache/singularity/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/fixedbitset-0.4.2/src/lib.rs /home/mhugo/.cache/singularity/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/fixedbitset-0.4.2/src/range.rs
-
-/home/mhugo/code/singularity/packages/singularity-code-analysis/.cargo-build/debug/deps/libfixedbitset-1f923cb1db34e7c6.rmeta: /home/mhugo/.cache/singularity/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/fixedbitset-0.4.2/src/lib.rs /home/mhugo/.cache/singularity/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/fixedbitset-0.4.2/src/range.rs
-
-/home/mhugo/.cache/singularity/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/fixedbitset-0.4.2/src/lib.rs:
-/home/mhugo/.cache/singularity/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/fixedbitset-0.4.2/src/range.rs:
