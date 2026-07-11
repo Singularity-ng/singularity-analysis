ffc63b20d8c34aeee75ab9c055921cb4dce71a68
ace-pm <agent-052a9fab-fb40-403b-bd08-d62bdb855c8b@letta.com>
2026-07-11 20:59:51 +0200

    git-auto-backup dirty snapshot
    repo: /home/mhugo/code/singularity-analysis-engine
    branch: main
    host: Mikki-Bunker
    time: 20260711T185809Z

--- mix.exs
+++ mix.exs
@@ -33,12 +33,6 @@ defmodule SingularityCodeAnalysis.MixProject do
 
       # Testing and coverage
       test_coverage: [tool: ExCoveralls],
-      preferred_cli_env: [
-        coveralls: :test,
-        "coveralls.detail": :test,
-        "coveralls.post": :test,
-        "coveralls.html": :test
-      ],
 
       # Dialyzer
       dialyzer: [
@@ -51,6 +45,17 @@ defmodule SingularityCodeAnalysis.MixProject do
 
   def application do
     []
+  end
+
+  def cli do
+    [
+      preferred_envs: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
+    ]
   end
 
   defp deps do
